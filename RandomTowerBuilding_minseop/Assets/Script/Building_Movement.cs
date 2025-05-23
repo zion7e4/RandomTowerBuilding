d0ec88520ae7d80e88290ab6c3dab0ed9cb7ab0e
vichead303 <vichead303@gmail.com>
2025-05-23 22:10:36 +0900

    회전
    회전

--- RandomTowerBuilding_minseop/Assets/Script/Building_Movement.cs
+++ RandomTowerBuilding_minseop/Assets/Script/Building_Movement.cs
@@ -5,20 +5,32 @@ public class Building_Movement : MonoBehaviour
 {
     [SerializeField]
     private float Speed = 5f;
+    [SerializeField]
+    private float rotateSpeed = 30f;
     private Vector3 moveDirection;
+    private Vector3 rotationDirection;
 
     private void Update()
     {
-        float x = Input.GetAxisRaw("Horizontal");  
+        float x = Input.GetAxisRaw("Horizontal"); //A, D / <-, -> Å° 
+        float z = Input.GetAxisRaw("Vertical");
         MoveTo(x);
+        Rotate(z);
         
     }
 
-    private void MoveTo(float x)
+    private void MoveTo(float x) //ÁÂ¿ì ÀÌµ¿
     {
         moveDirection = new Vector3(x, 0, 0);
 
         transform.position += moveDirection * Speed * Time.deltaTime;
+    }
+
+    private void Rotate(float z)
+    {
+        rotationDirection = new Vector3(0, 0, z);
+        transform.Rotate(rotationDirection * rotateSpeed * Time.deltaTime); 
+        
     }
 }
 
