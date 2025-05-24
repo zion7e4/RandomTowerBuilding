7dd9cf84fadaaead4ae343ef2bdba939df2f1e40
vichead303 <vichead303@gmail.com>
2025-05-24 19:03:28 +0900

    블록 변경, 블록 드랍 구현
    블록 변경, 블록 드랍 구현
    이제 할 거
    블록 스폰

--- RandomTowerBuilding_minseop/Assets/Script/Building_Movement.cs
+++ RandomTowerBuilding_minseop/Assets/Script/Building_Movement.cs
@@ -7,16 +7,27 @@ public class Building_Movement : MonoBehaviour
     private float Speed = 5f;
     [SerializeField]
     private float rotateSpeed = 30f;
+    private Rigidbody2D rigid2D;
     private Vector3 moveDirection;
     private Vector3 rotationDirection;
+    private bool isControllable = true;
+    public Building_Change buildingchange;
+
+    private void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
         float x = Input.GetAxisRaw("Horizontal"); //A, D / <-, -> 키 
         float z = Input.GetAxisRaw("Vertical");
-        MoveTo(x);
-        Rotate(z);
-        
+        if(isControllable)
+        {
+            MoveTo(x);
+            Rotate(z);
+            DropBlock();
+        }
     }
 
     private void MoveTo(float x) //좌우 이동
@@ -29,8 +40,25 @@ public class Building_Movement : MonoBehaviour
     private void Rotate(float z)
     {
         rotationDirection = new Vector3(0, 0, z);
-        transform.Rotate(rotationDirection * rotateSpeed * Time.deltaTime); 
-        
+        transform.Rotate(rotationDirection * rotateSpeed * Time.deltaTime);
+
+    }
+
+    private void DropBlock()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rigid2D.gravityScale = 1.0f;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground") || collision.gameObject.CompareTag("Block"))
+        {
+            isControllable = false;
+            rigid2D.linearVelocity = Vector2.zero; // 충돌 시 정지
+        }
     }
 }
 
