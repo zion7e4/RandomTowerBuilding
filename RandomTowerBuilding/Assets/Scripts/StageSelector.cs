e7770d9c199a4a6e77f2a7461542adebb9ed3114
20241112 이호정 <165740318+hojung3636@users.noreply.github.com>
2025-05-31 20:43:21 +0900

    ui 이미지 적용
    하하핳ㅎ

--- RandomTowerBuilding/Assets/Scripts/StageSelector.cs
+++ RandomTowerBuilding/Assets/Scripts/StageSelector.cs
@@ -13,6 +13,11 @@ public class StageSelector : MonoBehaviour
     public Image stage2Image;
     public Image stage3Image;
 
+    // 버튼의 Image 컴포넌트도 받아오기
+    private Image stage1ButtonImage;
+    private Image stage2ButtonImage;
+    private Image stage3ButtonImage;
+
     private string selectedStage = "";
     private bool stageSelected = false;
 
@@ -20,11 +25,20 @@ public class StageSelector : MonoBehaviour
     {
         GameStartButton.gameObject.SetActive(false);
 
+        stage1ButtonImage = stage1Button.GetComponent<Image>();
+        stage2ButtonImage = stage2Button.GetComponent<Image>();
+        stage3ButtonImage = stage3Button.GetComponent<Image>();
+
         stage1Button.onClick.AddListener(() => SelectStage("Stage1"));
         stage2Button.onClick.AddListener(() => SelectStage("Stage2"));
         stage3Button.onClick.AddListener(() => SelectStage("Stage3"));
 
         GameStartButton.onClick.AddListener(StartGame);
+
+        // 시작 시 이미지 모두 비활성화
+        stage1Image.gameObject.SetActive(false);
+        stage2Image.gameObject.SetActive(false);
+        stage3Image.gameObject.SetActive(false);
     }
 
     void SelectStage(string stageName)
@@ -35,10 +49,20 @@ public class StageSelector : MonoBehaviour
 
         GameStartButton.gameObject.SetActive(true);
 
-        // 이미지 음영 처리
-        SetStageImageOpacity(stage1Image, stageName == "Stage1" ? 0.5f : 1f);
-        SetStageImageOpacity(stage2Image, stageName == "Stage2" ? 0.5f : 1f);
-        SetStageImageOpacity(stage3Image, stageName == "Stage3" ? 0.5f : 1f);
+        // 선택된 이미지 활성화, 나머지 비활성화
+        stage1Image.gameObject.SetActive(stageName == "Stage1");
+        stage2Image.gameObject.SetActive(stageName == "Stage2");
+        stage3Image.gameObject.SetActive(stageName == "Stage3");
+
+        // 이미지 음영 처리: 선택된 것은 밝게, 나머지는 어둡게(알파 낮춤)
+        SetStageImageOpacity(stage1Image, stageName == "Stage1" ? 1f : 0.5f);
+        SetStageImageOpacity(stage2Image, stageName == "Stage2" ? 1f : 0.5f);
+        SetStageImageOpacity(stage3Image, stageName == "Stage3" ? 1f : 0.5f);
+
+        // 버튼 이미지도 음영 처리
+        SetStageImageOpacity(stage1ButtonImage, stageName == "Stage1" ? 1f : 0.5f);
+        SetStageImageOpacity(stage2ButtonImage, stageName == "Stage2" ? 1f : 0.5f);
+        SetStageImageOpacity(stage3ButtonImage, stageName == "Stage3" ? 1f : 0.5f);
     }
 
     void SetStageImageOpacity(Image img, float alpha)
