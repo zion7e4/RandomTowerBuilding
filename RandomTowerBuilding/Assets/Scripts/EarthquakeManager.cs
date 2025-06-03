f1fb976f1f41d01b53c26701e3b07556717dd58d
hy49 <gmldud7022@gmail.com>
2025-06-04 02:05:52 +0900

    지진 블록 흔들림 추가

--- RandomTowerBuilding/Assets/Scripts/EarthquakeManager.cs
+++ RandomTowerBuilding/Assets/Scripts/EarthquakeManager.cs
@@ -5,6 +5,7 @@ using UnityEngine.UI;
 public class EarthquakeManager : MonoBehaviour
 {
     Building_Change building_Change;
+    MainCameraController mainCameraController;
 
     public float earthquakeDelayMin = 1f;
     public float earthquakeDelayMax = 3f;
@@ -18,14 +19,20 @@ public class EarthquakeManager : MonoBehaviour
 
     private bool earthquakeTriggered = false;
 
+    Transform[] blocks;
+    Vector3[] originPosition;
+   
+    private bool isShaking = false;
 
     void Start()
     {
         building_Change = FindObjectOfType<Building_Change>();
+        mainCameraController = FindObjectOfType<MainCameraController>();
+
         if (warningUI != null)
         {
             warningUI.SetActive(false); // 시작 시 경고 UI 숨김
-        }
+        }        
     }
 
     void Update()
@@ -53,6 +60,13 @@ public class EarthquakeManager : MonoBehaviour
 
         // 지진 발생
         StartCoroutine(ShakeCamera());
+        OnEarthquake();
+
+        /*GameObject[] blocks = GameObject.FindGameObjectsWithTag("Block");
+        foreach (GameObject block in blocks)
+        {
+            block.SendMessage("OnEarthquake");
+        }*/
         //rumbleAudio.Play();
 
         // 블록 흔들리거나 밀리는 로직은 별도로 호출
@@ -64,6 +78,42 @@ public class EarthquakeManager : MonoBehaviour
 
         // 반복 가능하게 하려면 아래 라인 주석 처리
         // earthquakeTriggered = false;
+    }
+    public void OnEarthquake()
+    {
+        if (!isShaking)
+        {
+            StartCoroutine(ShakeBlock());
+        }
+    }
+    private IEnumerator ShakeBlock()
+    {
+        Debug.Log("Earthquake triggered, shaking blocks...");
+        isShaking = true;
+        float elapsed = 0f;
+
+        blocks = mainCameraController.allBlocks.ToArray(); // MainCameraController에서 블록 리스트 가져오기
+        originPosition = new Vector3[blocks.Length];
+        for(int i = 0; i < blocks.Length; i++)
+        {
+            originPosition[i] = blocks[i].position; // 각 블록의 초기 위치 저장
+        }
+
+        while (elapsed < shakeDuration)
+        {
+            for(int i = 0; i < blocks.Length; i++)
+            {
+                float offsetX = Random.Range(-shakeMagnitude, shakeMagnitude);
+                Vector3 newPos= originPosition[i] + new Vector3(offsetX, 0, 0); // 각 블록의 초기 위치 사용
+                blocks[i].transform.position = newPos;
+                Debug.Log("Block " + i + " moved to " + newPos);
+            }
+
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        isShaking = false;
     }
 
     IEnumerator ShakeCamera()
